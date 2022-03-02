2baeef8003f831025c5d107f53821affe4613af2
DmitryPatskalev <dimanord@yandex.ru>
2022-03-02 19:39:00 +0300

    commit

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Arms_of_the_City_of_Manchester.svg/150px-Arms_of_the_City_of_Manchester.svg.png" />
+    </div>
+  );
+};
+export default Header;
