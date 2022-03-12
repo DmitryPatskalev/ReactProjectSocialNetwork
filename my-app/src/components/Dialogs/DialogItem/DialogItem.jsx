c5bbaf75be440448bafe5a98f12110ecfecf4674
DmitryPatskalev <dimanord@yandex.ru>
2022-03-12 10:25:56 +0300

    commit

--- my-app/src/components/Dialogs/DialogItem/DialogItem.jsx
+++ my-app/src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from "react-router-dom";
 import css from "./../Dialogs.module.css";
+import kitty from "./../../Photo/kitty.jpg";
 
 const DialogItem = (props) => {
   let path = "/dialogs/" + props.id;
   return (
     <div className={css.dialog}>
+      <span className={css.cat}>
+        <img src={kitty} />
+      </span>
       <NavLink to={path}>{props.name}</NavLink>
     </div>
   );
 };
+
 export default DialogItem;
--- my-app/src/components/Profile/MyPosts/MyPost.jsx
+++ my-app/src/components/Profile/MyPosts/MyPost.jsx
@@ -1,20 +1,27 @@
 import Post from "./Post/Post";
 import css from "./MyPost.module.css";
+import React from "react";
 
 const MyPost = (props) => {
   let postsElement = props.posts.map((p) => (
     <Post message={p.message} like={p.like} />
   ));
 
+  let postAdd = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
+  let newPostElement = React.useRef();
+
   return (
     <div className={css.postBlock}>
       <h3>My Post</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Click me</button>
+          <button onClick={postAdd}>Click me</button>
         </div>
       </div>
       <div className={css.posts}>{postsElement}</div>
