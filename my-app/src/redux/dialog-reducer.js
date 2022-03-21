e7ed419597a133d21c963b2796c22ae90cd215f0
DmitryPatskalev <dimanord@yandex.ru>
2022-03-21 17:55:16 +0300

    commit

--- my-app/src/redux/dialog-reducer.js
+++ my-app/src/redux/dialog-reducer.js
@@ -2,14 +2,18 @@ const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
 const SEND_MESSAGE = "SEND-MESSAGE";
 
 const dialogReducer = (state, action) => {
-  if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-    state.newMessageBody = action.body;
-  } else if (action.type === SEND_MESSAGE) {
-    let body = state.newMessageBody;
-    state.newMessageBody = "";
-    state.messages.push({ id: 4, message: body });
+  switch (action.type) {
+    case UPDATE_NEW_MESSAGE_BODY:
+      state.newMessageBody = action.body;
+      return state;
+    case SEND_MESSAGE:
+      let body = state.newMessageBody;
+      state.newMessageBody = "";
+      state.messages.push({ id: 4, message: body });
+      return state;
+    default:
+      return state;
   }
-  return state;
 };
 
 export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
