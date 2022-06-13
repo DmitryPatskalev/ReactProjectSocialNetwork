1eb6ddbaf74b778f2ddb2ebdb0f43a769bceb7ce
DmitryPatskalev <dimanord@yandex.ru>
2022-06-13 10:08:44 +0300

    commit

--- my-app/src/redux/redux-store.js
+++ my-app/src/redux/redux-store.js
@@ -2,7 +2,7 @@ import profileReducer from "./profile-reducer";
 import dialogReducer from "./dialog-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import userReducer from "./users-reducer";
-import { combineReducers, createStore } from "redux";
+import { combineReducers, legacy_createStore as createStore } from "redux";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
