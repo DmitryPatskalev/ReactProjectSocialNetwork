414c971cb927137a13a6291b30b69c9a3e112655
DmitryPatskalev <dimanord@yandex.ru>
2022-06-21 12:28:31 +0300

    commit

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -4,63 +4,65 @@ import * as axios from "axios";
 
 
 const Users = (props) => {
+   const getUsers = () => {
+      if (props.users.length === 0) {
+         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(responce => {
+            props.setUsers(responce.data.items)
+         })
 
-   if (props.users.length === 0) {
-      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(responce=>{
-         props.setUsers(responce.data.items)
-      })
-
-      // props.setUsers([
-      //    {
-      //       id: v1(),
-      //       photoUrl: "https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
-      //       followed: false,
-      //       fullName: "Dmitry",
-      //       status: "I am Dude",
-      //       location: {city: "Minsk", country: "Belarus"},
-      //    },
-      //    {
-      //       id: v1(),
-      //       photoUrl:"https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
-      //       followed: true,
-      //       fullName: "Tania",
-      //       status: "I am cool girl",
-      //       location: {city: "Minsk", country: "Belarus"},
-      //    },
-      //    {
-      //       id: v1(),
-      //       photoUrl:"https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
-      //       followed: false,
-      //       fullName: "Danik",
-      //       status: "I am little boy",
-      //       location: {city: "Minsk", country: "Belarus"},
-      //    },
-      //    {
-      //       id: v1(),
-      //       photoUrl:"https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
-      //       followed: true,
-      //       fullName: "Igor",
-      //       status: "I am big man",
-      //       location: {city: "Lviv", country: "Ukraine"},
-      //    },
-      //    {
-      //       id: v1(),
-      //       photoUrl:"https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
-      //       followed: false,
-      //       fullName: "Julia",
-      //       status: "I am happy!",
-      //       location: {city: "Chicago", country: "USA"},
-      //    },
-      //
-      // ])
+         // props.setUsers([
+         //    {
+         //       id: v1(),
+         //       photoUrl: "https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
+         //       followed: false,
+         //       fullName: "Dmitry",
+         //       status: "I am Dude",
+         //       location: {city: "Minsk", country: "Belarus"},
+         //    },
+         //    {
+         //       id: v1(),
+         //       photoUrl:"https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
+         //       followed: true,
+         //       fullName: "Tania",
+         //       status: "I am cool girl",
+         //       location: {city: "Minsk", country: "Belarus"},
+         //    },
+         //    {
+         //       id: v1(),
+         //       photoUrl:"https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
+         //       followed: false,
+         //       fullName: "Danik",
+         //       status: "I am little boy",
+         //       location: {city: "Minsk", country: "Belarus"},
+         //    },
+         //    {
+         //       id: v1(),
+         //       photoUrl:"https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
+         //       followed: true,
+         //       fullName: "Igor",
+         //       status: "I am big man",
+         //       location: {city: "Lviv", country: "Ukraine"},
+         //    },
+         //    {
+         //       id: v1(),
+         //       photoUrl:"https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/mission_-impossible-7-crew-calls-tom-cruise-a-nightmare.jpg",
+         //       followed: false,
+         //       fullName: "Julia",
+         //       status: "I am happy!",
+         //       location: {city: "Chicago", country: "USA"},
+         //    },
+         //
+         // ])
+      }
    }
 
-   return <div className={css.usersList}>
 
+   return <div className={css.usersList}>
+      <button onClick={getUsers}>get Users</button>
       {props.users.map((u) => <div key={u.id}>
   <span>
       <div>
-        <img src={u.photos.small !==null? u.photos.small:userPhoto} className={css.userPhoto} alt='ava'/>
+        <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={css.userPhoto} alt='ava'/>
       </div>
       <div>
          {u.followed ?
