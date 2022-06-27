dfda3a6f44d9a555cca3f63288bac0267d90dc38
DmitryPatskalev <dimanord@yandex.ru>
2022-06-27 13:26:52 +0300

    commit

--- my-app/src/components/Users/Users.jsx
+++ my-app/src/components/Users/Users.jsx
@@ -11,12 +11,32 @@ class Users extends React.Component {
       })
    }
 
+    onPageChanged = (pageNumber)=>{
+      this.props.setCurrentPage(pageNumber)
+       axios.get('https://social-network.samuraijs.com/api/1.0/users').then(responce => {
+          this.props.setTotalUsersCount(responce.data.totalCount)
+       })
+   }
    render() {
+      let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+      let pages = []
+      for(let i = 1; i <= pagesCount; i++){
+         pages.push(i)
+      }
+
+
       return <div className={css.usersList}>
+         <div>
+            {pages.map((elem, index)=>{
+               return <span key={index}
+                            className={this.props.currentPage === elem ? css.selectedPage:undefined}
+                            onClick={(e)=>this.onPageChanged(elem)}
+               >{elem}</span>
+            })}
+         </div>
 
          {this.props.users.map((u) => <div key={u.id}>
-  <span>
-      <div >
+      <div>
         <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={css.userPhoto} alt='ava'/>
       </div>
       <div>
@@ -24,7 +44,6 @@ class Users extends React.Component {
              <button onClick={() => (this.props.unfollow(u.id))}>Unfollow</button> :
              <button onClick={() => (this.props.follow(u.id))}>Follow</button>}
       </div>
-  </span>
             <span>
             <div>{u.name}</div>
             <div>{u.status}</div>
