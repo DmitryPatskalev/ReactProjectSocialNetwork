a1d83c9dce4c360f104f6ae46690e3698b7a53ab
DmitryPatskalev <dimanord@yandex.ru>
2022-07-07 17:28:03 +0300

    commit

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import css from "./Header.module.css";
 import coatOfArms from "./../Photo/CoatOfArms.jpg";
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className={css.header}>
-      <img src={coatOfArms} alt='coat'  />
+      <img src={coatOfArms} alt='coat'/>
+      <div className={css.loginBlock}>
+        {props.isAuth?props.login:
+          <NavLink to={'/login'}>Login</NavLink>
+        }
+      </div>
     </div>
   );
 };
--- my-app/src/redux/redux-store.js
+++ my-app/src/redux/redux-store.js
@@ -2,13 +2,16 @@ import profileReducer from "./profile-reducer";
 import dialogReducer from "./dialog-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
-import { combineReducers, createStore } from "redux";
+import {combineReducers, createStore} from "redux";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
+  auth: authReducer
+
 });
 let store = createStore(reducers);
 
